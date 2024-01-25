61dedfbb12aaa7929480a613460a45da0c476cbb
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-01-25 20:39:39 +0200

    Another variant of this task

--- Sets/remove_duplicates/main.py
+++ Sets/remove_duplicates/main.py
@@ -1,5 +1,6 @@
 def remove_duplicates(spells):
-    filter_set = set()
-    for spell in spells:
-        filter_set.add(spell)
-    return list(filter_set)
+    return list(set(spells))
+    # filter_set = set()
+    # for spell in spells:
+    #     filter_set.add(spell)
+    # return list(filter_set)
